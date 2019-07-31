fc6961eb7a9ea13c4d15222082bb623537e94d9c
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-31 18:19:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,27 +4,27 @@ class Show < ActiveRecord::Base
     Show.maximum(:rating)
   end 
   
-  def most_popular_show 
+  def Show::most_popular_show 
     Show.highest_rating
   end 
   
-  def lowest_rating
+  def Show::lowest_rating
     Show.minimum(:rating)
   end 
   
-  def least_popular_show 
+  def Show::least_popular_show 
     Show.lowest_rating
   end 
   
-  def ratings_sum 
+  def Show::ratings_sum 
     Show.sum(:rating)
   end 
   
-  def popular_shows 
+  def Show::popular_shows 
     Show.where("rating > ?", 5)
   end 
   
-  def shows_by_alphabetical_order 
+  def Show::shows_by_alphabetical_order 
     Show.order(:name)
   end 
 end 
