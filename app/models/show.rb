f13a7586416c4a1ecd5f5f61e93f8b74db601483
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-31 18:23:33 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
   end 
   
   def Show::least_popular_show 
-    Show.where("rating = ?", '#{Show::lowest_rating}')
+    Show.find_by("rating = ?", '#{Show::lowest_rating}')
   end 
   
   def Show::ratings_sum 
