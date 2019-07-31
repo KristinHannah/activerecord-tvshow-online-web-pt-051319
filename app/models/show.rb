0eee36b309183cca43793097e683cc4dcec546d2
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-31 18:16:40 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,16 @@ class Show < ActiveRecord::Base
   def highest_rating 
     Show.maximum(:rating)
   end 
+  
+  def most_popular_show 
+    Show.highest_rating
+  end 
+  
+  def lowest_rating
+    Show.minimum(:rating)
+  end 
+  
+  def least_popular_show 
+    Show.lowest_rating
+  end 
 end 
